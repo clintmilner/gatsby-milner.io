4ac1494709b8defd103a4d71c50d98c27825e3fe
Clint Milner <clinton.milner@rebasoft.net>
2018-09-13 20:46:18 +0100

    scss and page 3 testing

---
+++ src/pages/page-3.js
@@ -0,0 +1,19 @@
+/**
+ * User: clint
+ * Date: 13/09/2018
+ * Time: 20:24
+ *
+ * Rebasoft - Network Intelligence
+ */
+import React from 'react'
+import Link from 'gatsby-link'
+
+const ThirdPage = () => (
+  <div>
+    <h1>Hi from the third page</h1>
+    <p>Welcome to page 3</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default ThirdPage
